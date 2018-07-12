29b12b7a23476a269bbb2bf4b52ae83dbaa57f19
akshya <sakshyaraghavan@gmail.com>
2018-07-11 20:49:14 -0500

    First commit

---
+++ application/views/bottomcol_view.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<footer>
+	<p>
+		<i>Copyright &copy 2018 Pacific Trails Resort</i>
+		<br>
+		<i><a href="mailto:akshya@srinivasaraghavan.com">akshya@srinivasaraghavan.com</a></i>
+	</p>
+
+</footer>
+
+</div>
+</div>
+</body>
+</html>
